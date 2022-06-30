f0039638760d544aa0b1c61d9f6bbb7b04f8cc81
Mark Robert Miller <markrmiller@apache.org>
2022-06-30 16:27:58 -0500

    Update Solr version, build improvement for some envs, include missing solrj dep (#28)

--- cluster.sh
+++ cluster.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 kafkaBase="https://archive.apache.org/dist/kafka/2.8.1"
-solrBase="https://dlcdn.apache.org/lucene/solr/8.11.1"
+solrBase="https://dlcdn.apache.org/lucene/solr/8.11.2"
 
 kafka="kafka_2.12-2.8.1"
-solr="solr-8.11.1"
+solr="solr-8.11.2"
 
 if [ ! -d cluster ]
 then
