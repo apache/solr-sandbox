388a6833efde40e49b231a6b065726068ac123cd
Bruno Roustant <bruno.roustant@gmail.com>
2023-09-11 17:47:25 +0200

    Add CharStreamEncrypter.

---
+++ encryption/src/main/java/org/apache/solr/encryption/crypto/CharStreamEncrypter.java
@@ -0,0 +1,188 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.solr.encryption.crypto;
+
+import org.apache.commons.codec.binary.Base64InputStream;
+import org.apache.commons.codec.binary.Base64OutputStream;
+import org.apache.commons.io.input.ReaderInputStream;
+import org.apache.commons.io.output.AppendableWriter;
+import org.apache.commons.io.output.WriterOutputStream;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Encrypts a character stream to a base 64 encoding compatible with JSON.
+ * <p>
+ * The whole encryption and base 64 encoding process is streamed, with no large
+ * buffers allocated. The encryption transformation is AES/CTR/NoPadding.
+ * A secure random IV is generated for each encryption and appended as the first
+ * appended chars.
+ */
+public class CharStreamEncrypter {
+
+  private static final int BUFFER_MIN_SIZE = 128;
+  private static final int BUFFER_MAX_SIZE = 8192;
+
+  private final AesCtrEncrypterFactory factory;
+
+  public CharStreamEncrypter(AesCtrEncrypterFactory factory) {
+    this.factory = factory;
+  }
+
+  /**
+   * Encrypts an input string to base 64 characters compatible with JSON.
+   *
+   * @param key    AES key, can either 16, 24 or 32 bytes.
+   * @param output where to append the encrypted base 64 chars.
+   * @throws IOException propagates any exception thrown when appending to the output.
+   */
+  public void encrypt(String input, byte[] key, Appendable output)
+    throws IOException {
+    encrypt(new StringReader(input), input.length(), key, output);
+  }
+
+  /**
+   * Encrypts a char reader stream to base 64 characters compatible with JSON.
+   *
+   * @param inputSizeHint optional hint for the input size; or -1 if unknown.
+   * @param key           AES key, can either 16, 24 or 32 bytes.
+   * @param output        where to append the encrypted base 64 chars.
+   * @throws IOException propagates any exception thrown when appending to the output.
+   */
+  public void encrypt(Reader inputReader, int inputSizeHint, byte[] key, Appendable output)
+    throws IOException {
+    // Don't use jdk Base64.getEncoder().wrap() because it's buggy.
+    int bufferSize = getBufferSize(inputSizeHint);
+    try (OutputStreamWriter encryptedOutputWriter =
+           new OutputStreamWriter(
+             new EncryptingOutputStream(
+               new Base64OutputStream(
+                 new LightWriterOutputStream(
+                   toWriter(output),
+                   StandardCharsets.ISO_8859_1,
+                   bufferSize
+                 )
+               ),
+               key,
+               factory
+             ),
+             StandardCharsets.UTF_8
+           )
+    ) {
+      transfer(inputReader,
+               encryptedOutputWriter,
+               bufferSize
+      );
+    }
+  }
+
+  /**
+   * Decrypts an input string previously encrypted with {@link #encrypt}.
+   *
+   * @param key    AES key, can either 16, 24 or 32 bytes.
+   * @param output where to append the decrypted chars.
+   * @throws IOException propagates any exception thrown when appending to the output.
+   */
+  public void decrypt(String input, byte[] key, Appendable output)
+    throws IOException {
+    decrypt(new StringReader(input), input.length(), key, output);
+  }
+
+  /**
+   * Decrypts a char reader stream previously encrypted with {@link #encrypt}.
+   *
+   * @param inputSizeHint optional hint for the input size; or -1 if unknown.
+   * @param key           AES key, can either 16, 24 or 32 bytes.
+   * @param output        where to append the decrypted chars.
+   * @throws IOException propagates any exception thrown when appending to the output.
+   */
+  public void decrypt(Reader inputReader, int inputSizeHint, byte[] key, Appendable output)
+    throws IOException {
+    // Don't use jdk Base64.getDecoder().wrap() because it's buggy.
+    int bufferSize = getBufferSize(inputSizeHint);
+    try (InputStreamReader decryptedInputReader =
+           new InputStreamReader(
+             new DecryptingInputStream(
+               new Base64InputStream(
+                 new ReaderInputStream(
+                   inputReader,
+                   StandardCharsets.ISO_8859_1,
+                   bufferSize
+                 )
+               ),
+               key,
+               factory
+             ),
+             StandardCharsets.UTF_8
+           )
+    ) {
+      transfer(decryptedInputReader,
+               toWriter(output),
+               bufferSize
+      );
+    }
+  }
+
+  private static int getBufferSize(int inputSizeHint) {
+    return inputSizeHint < 0 ? BUFFER_MAX_SIZE
+      : Math.min(Math.max(inputSizeHint / 16, BUFFER_MIN_SIZE), BUFFER_MAX_SIZE);
+  }
+
+  private static Writer toWriter(Appendable appendable) {
+    return appendable instanceof Writer ? (Writer) appendable : new AppendableWriter<>(appendable);
+  }
+
+  /**
+   * Similar to {@link Reader#transferTo(Writer)} with a provided buffer size.
+   */
+  private static void transfer(Reader input, Writer output, int bufferSize)
+    throws IOException {
+    char[] buffer = new char[bufferSize];
+    int nRead;
+    while ((nRead = input.read(buffer, 0, bufferSize)) >= 0) {
+      output.write(buffer, 0, nRead);
+    }
+  }
+
+  /**
+   * Same as {@link WriterOutputStream} without creating a buffer for each
+   * call to {@link #write(int)}.
+   */
+  private static class LightWriterOutputStream extends WriterOutputStream {
+
+    private final byte[] oneByteBuf = new byte[1];
+
+    public LightWriterOutputStream(Writer writer,
+                                   Charset charset,
+                                   int bufferSize) {
+      super(writer, charset, bufferSize, false);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+      oneByteBuf[0] = (byte) b;
+      write(oneByteBuf, 0, 1);
+    }
+  }
+}
---
+++ encryption/src/main/java/org/apache/solr/encryption/crypto/DecryptingInputStream.java
@@ -0,0 +1,162 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.solr.encryption.crypto;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+import static org.apache.solr.encryption.crypto.AesCtrUtil.AES_BLOCK_SIZE;
+import static org.apache.solr.encryption.crypto.AesCtrUtil.IV_LENGTH;
+
+/**
+ * {@link InputStream} that reads from a delegate {@link InputStream} and decrypts data on the fly.
+ * <p>The encryption transformation is AES/CTR/NoPadding. It decrypts the data previously encrypted
+ * with an {@link EncryptingOutputStream}.
+ * <p>It first reads the CTR Initialization Vector (IV). This random IV is not encrypted. Then it
+ * can decrypt the rest of the file.
+ *
+ * @see EncryptingOutputStream
+ * @see AesCtrEncrypter
+ */
+public class DecryptingInputStream extends InputStream {
+
+  /**
+   * Must be a multiple of {@link AesCtrUtil#AES_BLOCK_SIZE}.
+   * Benchmarks showed that 6 x {@link AesCtrUtil#AES_BLOCK_SIZE} is a good buffer size.
+   */
+  private static final int BUFFER_CAPACITY = 6 * AES_BLOCK_SIZE; // 96 B
+
+  private final InputStream inputStream;
+  private final AesCtrEncrypter encrypter;
+  private final ByteBuffer inBuffer;
+  private final ByteBuffer outBuffer;
+  private final byte[] inArray;
+  private final byte[] oneByteBuf;
+  private boolean closed;
+
+  /**
+   * @param inputStream The delegate {@link InputStream} to read and decrypt data from.
+   * @param key         The encryption key secret. It is cloned internally, its content
+   *                    is not modified, and no reference to it is kept.
+   * @param factory     The factory to use to create one instance of {@link AesCtrEncrypter}.
+   */
+  public DecryptingInputStream(InputStream inputStream, byte[] key, AesCtrEncrypterFactory factory) throws IOException {
+    this.inputStream = inputStream;
+    this.encrypter = createEncrypter(inputStream, key, factory);
+    encrypter.init(0);
+    inBuffer = ByteBuffer.allocate(getBufferCapacity());
+    outBuffer = ByteBuffer.allocate(getBufferCapacity() + AES_BLOCK_SIZE);
+    outBuffer.limit(0);
+    assert inBuffer.hasArray() && outBuffer.hasArray();
+    assert inBuffer.arrayOffset() == 0;
+    inArray = inBuffer.array();
+    oneByteBuf = new byte[1];
+  }
+
+  /**
+   * Creates the {@link AesCtrEncrypter} based on the secret key and the IV at the beginning
+   * of the input stream.
+   */
+  private static AesCtrEncrypter createEncrypter(InputStream inputStream,
+                                                 byte[] key,
+                                                 AesCtrEncrypterFactory factory)
+    throws IOException {
+    byte[] iv = new byte[IV_LENGTH];
+    int n = inputStream.read(iv, 0, iv.length);
+    if (n != iv.length) {
+      throw new IOException("Missing IV");
+    }
+    return factory.create(key, iv);
+  }
+
+  /**
+   * Gets the buffer capacity. It must be a multiple of {@link AesCtrUtil#AES_BLOCK_SIZE}.
+   */
+  protected int getBufferCapacity() {
+    return BUFFER_CAPACITY;
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (!closed) {
+      closed = true;
+      inputStream.close();
+    }
+  }
+
+  @Override
+  public int read() throws IOException {
+    int n = read(oneByteBuf, 0, 1);
+    return n == -1 ? -1 : oneByteBuf[0];
+  }
+
+  @Override
+  public int read(byte[] b, int offset, int length) throws IOException {
+    if (offset < 0 || length < 0 || offset + length > b.length) {
+      throw new IllegalArgumentException(
+        "Invalid read buffer parameters (offset=" + offset + ", length=" + length
+          + ", arrayLength=" + b.length + ")");
+    }
+    int numRead = 0;
+    while (length > 0) {
+      // Transfer decrypted bytes from outBuffer.
+      int outRemaining = outBuffer.remaining();
+      if (outRemaining > 0) {
+        if (length <= outRemaining) {
+          outBuffer.get(b, offset, length);
+          numRead += length;
+          return numRead;
+        }
+        outBuffer.get(b, offset, outRemaining);
+        numRead += outRemaining;
+        assert outBuffer.remaining() == 0;
+        offset += outRemaining;
+        length -= outRemaining;
+      }
+
+      if (!readToFillBuffer(length)) {
+        return numRead == 0 ? -1 : numRead;
+      }
+      decryptBuffer();
+    }
+    return numRead;
+  }
+
+  private boolean readToFillBuffer(int length) throws IOException {
+    assert length > 0;
+    int inRemaining = inBuffer.remaining();
+    if (inRemaining > 0) {
+      int position = inBuffer.position();
+      int numBytesToRead = Math.min(inRemaining, length);
+      int n = inputStream.read(inArray, position, numBytesToRead);
+      if (n == -1) {
+        return false;
+      }
+      inBuffer.position(position + n);
+    }
+    return true;
+  }
+
+  private void decryptBuffer() {
+    inBuffer.flip();
+    outBuffer.clear();
+    encrypter.process(inBuffer, outBuffer);
+    inBuffer.clear();
+    outBuffer.flip();
+  }
+}
