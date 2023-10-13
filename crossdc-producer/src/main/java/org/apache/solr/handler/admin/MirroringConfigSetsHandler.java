949c1b0043d62188d11b8baaafda1bfff4c690af
Andrzej Bialecki <ab@apache.org>
2023-10-13 16:07:00 +0200

    Add license headers.

--- crossdc-producer/src/main/java/org/apache/solr/handler/admin/MirroringConfigSetsHandler.java
+++ crossdc-producer/src/main/java/org/apache/solr/handler/admin/MirroringConfigSetsHandler.java
@@ -1,10 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.solr.handler.admin;
 
 import com.google.common.annotations.VisibleForTesting;
 import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.common.cloud.SolrZkClient;
 import org.apache.solr.common.util.ContentStream;
 import org.apache.solr.core.CoreContainer;
-import org.apache.solr.crossdc.common.ConfigProperty;
+import org.apache.solr.crossdc.common.ConfUtil;
 import org.apache.solr.crossdc.common.CrossDcConstants;
 import org.apache.solr.crossdc.common.KafkaCrossDcConf;
 import org.apache.solr.crossdc.common.KafkaMirroringSink;
@@ -12,7 +29,6 @@ import org.apache.solr.crossdc.common.MirroredSolrRequest;
 import org.apache.solr.request.LocalSolrQueryRequest;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.response.SolrQueryResponse;
-import org.apache.solr.update.processor.ConfUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,9 +59,11 @@ public class MirroringConfigSetsHandler extends ConfigSetsHandler {
     if (sink == null) {
       Map<String, Object> properties = new HashMap<>();
       try {
+        SolrZkClient zkClient = null;
         if (coreContainer.getZkController() != null) {
-          ConfUtil.fillProperties(coreContainer.getZkController().getZkClient(), properties);
+          zkClient = coreContainer.getZkController().getZkClient();
         }
+        ConfUtil.fillProperties(zkClient, properties);
         KafkaCrossDcConf conf = new KafkaCrossDcConf(properties);
         this.sink = new KafkaMirroringSink(conf);
       } catch (Exception e) {
