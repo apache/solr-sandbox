fe86764adfdc465dc0d1a9b54576320f3b18c478
Andrzej Białecki <ab@apache.org>
2023-10-13 14:37:42 +0200

    Add support for mirroring Collection Admin commands. (#68)

--- crossdc-producer/src/main/java/org/apache/solr/update/processor/KafkaRequestMirroringHandler.java
+++ crossdc-producer/src/main/java/org/apache/solr/update/processor/KafkaRequestMirroringHandler.java
@@ -49,6 +49,6 @@ public class KafkaRequestMirroringHandler implements RequestMirroringHandler {
             log.trace("submit update to sink docs={}, deletes={}, params={}", request.getDocuments(), request.getDeleteById(), request.getParams());
         }
         // TODO: Enforce external version constraint for consistent update replication (cross-cluster)
-        sink.submit(new MirroredSolrRequest(1, request, TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis())));
+        sink.submit(new MirroredSolrRequest(MirroredSolrRequest.Type.UPDATE, 1, request, TimeUnit.MILLISECONDS.toNanos(System.currentTimeMillis())));
     }
 }
