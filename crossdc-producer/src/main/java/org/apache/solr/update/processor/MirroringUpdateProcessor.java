47e941aaf61ba73e95294027392913e517c931ed
Marcin Górski <marcinxgorski@gmail.com>
2023-11-27 12:41:51 +0100

    Producer metrics (#87)

--- crossdc-producer/src/main/java/org/apache/solr/update/processor/MirroringUpdateProcessor.java
+++ crossdc-producer/src/main/java/org/apache/solr/update/processor/MirroringUpdateProcessor.java
@@ -44,6 +44,7 @@ public class MirroringUpdateProcessor extends UpdateRequestProcessor {
    */
   private final boolean doMirroring;
   final RequestMirroringHandler requestMirroringHandler;
+  final ProducerMetrics producerMetrics;
 
   /**
    * The mirrored request starts as null, gets created and appended to at each process() call,
@@ -75,13 +76,15 @@ public class MirroringUpdateProcessor extends UpdateRequestProcessor {
    */
   private DistributedUpdateProcessor.DistribPhase distribPhase;
 
-  public MirroringUpdateProcessor(final UpdateRequestProcessor next, boolean doMirroring,
+  public MirroringUpdateProcessor(final UpdateRequestProcessor next,
+                                  boolean doMirroring,
       final boolean indexUnmirrorableDocs,
       final boolean mirrorCommits,
       final long maxMirroringBatchSizeBytes,
       final SolrParams mirroredReqParams,
       final DistributedUpdateProcessor.DistribPhase distribPhase,
-      final RequestMirroringHandler requestMirroringHandler) {
+      final RequestMirroringHandler requestMirroringHandler,
+      final ProducerMetrics producerMetrics) {
     super(next);
     this.doMirroring = doMirroring;
     this.indexUnmirrorableDocs = indexUnmirrorableDocs;
@@ -90,7 +93,7 @@ public class MirroringUpdateProcessor extends UpdateRequestProcessor {
     this.mirrorParams = mirroredReqParams;
     this.distribPhase = distribPhase;
     this.requestMirroringHandler = requestMirroringHandler;
-
+    this.producerMetrics = producerMetrics;
     // Find the downstream distributed update processor
 
   }
@@ -107,18 +110,26 @@ public class MirroringUpdateProcessor extends UpdateRequestProcessor {
     doc.removeField(CommonParams.VERSION_FIELD); // strip internal doc version
     final long estimatedDocSizeInBytes = ObjectSizeEstimator.estimate(doc);
     log.info("estimated doc size is {} bytes, max size is {}", estimatedDocSizeInBytes, maxMirroringDocSizeBytes);
+    producerMetrics.getDocumentSize().update(estimatedDocSizeInBytes);
     final boolean tooLargeForKafka = estimatedDocSizeInBytes > maxMirroringDocSizeBytes;
     if (tooLargeForKafka && !indexUnmirrorableDocs) {
       throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, "Update exceeds the doc-size limit and is unmirrorable. id="
 
           + cmd.getPrintableId() + " doc size=" + estimatedDocSizeInBytes + " maxDocSize=" + maxMirroringDocSizeBytes);
     } else if (tooLargeForKafka) {
+      producerMetrics.getDocumentTooLarge().inc();
       log.warn(
           "Skipping mirroring of doc {} as it exceeds the doc-size limit ({} bytes) and is unmirrorable. doc size={}",
           cmd.getPrintableId(), maxMirroringDocSizeBytes, estimatedDocSizeInBytes);
     }
 
-    super.processAdd(cmd); // let this throw to prevent mirroring invalid reqs
+    try {
+      super.processAdd(cmd); // let this throw to prevent mirroring invalid reqs
+      producerMetrics.getLocal().inc();
+    } catch (IOException exception) {
+      producerMetrics.getLocalError().inc();
+      throw exception;
+    }
 
     // submit only from the leader shards so we mirror each doc once
     boolean isLeader = isLeader(cmd.getReq(),  cmd.getIndexedIdStr(), null, cmd.getSolrInputDocument());
@@ -128,8 +139,10 @@ public class MirroringUpdateProcessor extends UpdateRequestProcessor {
 
       try {
         requestMirroringHandler.mirror(mirrorRequest);
+        producerMetrics.getSubmitted().inc();
       } catch (Exception e) {
         log.error("mirror submit failed", e);
+        producerMetrics.getSubmitError().inc();
         throw new SolrException(SERVER_ERROR, "mirror submit failed", e);
       }
     }
--- crossdc-producer/src/main/java/org/apache/solr/update/processor/MirroringUpdateRequestProcessorFactory.java
+++ crossdc-producer/src/main/java/org/apache/solr/update/processor/MirroringUpdateRequestProcessorFactory.java
@@ -66,7 +66,7 @@ public class MirroringUpdateRequestProcessorFactory extends UpdateRequestProcess
 
     /** This is instantiated in inform(SolrCore) and then shared by all processor instances - visible for testing */
     private volatile KafkaRequestMirroringHandler mirroringHandler;
-
+    private volatile ProducerMetrics producerMetrics;
 
     private boolean enabled = true;
 
@@ -190,6 +190,7 @@ public class MirroringUpdateRequestProcessorFactory extends UpdateRequestProcess
         Closer closer = new Closer(sink);
         core.addCloseHook(new MyCloseHook(closer));
 
+        producerMetrics = new ProducerMetrics(core.getSolrMetricsContext().getChildContext(this), core);
         mirroringHandler = new KafkaRequestMirroringHandler(sink);
     }
 
@@ -250,7 +251,7 @@ public class MirroringUpdateRequestProcessorFactory extends UpdateRequestProcess
         }
 
         return new MirroringUpdateProcessor(next, doMirroring, indexUnmirrorableDocs, mirrorCommits, maxMirroringBatchSizeBytes, mirroredParams,
-                DistribPhase.parseParam(req.getParams().get(DISTRIB_UPDATE_PARAM)), doMirroring ? mirroringHandler : null);
+                DistribPhase.parseParam(req.getParams().get(DISTRIB_UPDATE_PARAM)), doMirroring ? mirroringHandler : null, producerMetrics);
     }
 
     public static class NoOpUpdateRequestProcessor extends UpdateRequestProcessor {
